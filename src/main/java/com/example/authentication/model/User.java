ce16171ef7e3c90ded86fd1b138c867fae9e9558
morsko1 <morssko1@gmail.com>
2023-06-21 12:58:20 +0600

    implement JwtService and UserDetails

--- src/main/java/com/example/authentication/model/User.java
+++ src/main/java/com/example/authentication/model/User.java
@@ -1,5 +1,11 @@
 package com.example.authentication.model;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,7 +15,7 @@ import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -19,6 +25,8 @@ public class User {
 
   @NotBlank
   private String password;
+
+  public User() {}
 
   public User(String username, String password) {
     this.username = username;
@@ -47,5 +55,30 @@ public class User {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.emptySet();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 }
